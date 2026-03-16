c36d811a27843f3a12278760af15114f63c30e54
Sreyash Satpathy <sreyash.s@outlook.com>
2026-03-16 19:28:21 +0530

    setup celery queues and redis

--- series/models.py
+++ series/models.py
@@ -1,10 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-class Series(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.TextField()
-    release_date = models.DateField()
-    genre = models.CharField(max_length=100)
-    rating = models.FloatField()
